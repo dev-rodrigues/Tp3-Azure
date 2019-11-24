a2d7c148fb3e9ca7565eeb504c3f24fcdad4451d
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-23 23:31:31 -0300

    mais testes

---
+++ Test/AmigoRepositoryTest.cs
@@ -0,0 +1,43 @@
+﻿using API.Service;
+using Core.Model;
+using Core.Repository;
+using Data.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test {
+    [TestClass]
+    public class AmigoRepositoryTest {
+
+        private IAmigo repositorio = ServiceLocator.GetInstanceOf<AmigoRepository>();
+
+        [TestMethod]
+        public void TestSalvarAmigo_GeraId() {
+            
+            var amigo = new Amigo();
+            
+            var amigoEncontrado = SalvarAmigoTest(amigo);
+            
+            Assert.AreEqual(amigo.Id, amigoEncontrado.Id);
+        }
+
+        [TestMethod]
+        public void Deve_RetornarNome_Quando_SalvarAmigo() {
+            var amigo = new Amigo();
+            amigo.Nome = "carlos";
+
+            var amigoEncontrado = SalvarAmigoTest(amigo);
+
+            Assert.AreEqual("carlos", amigoEncontrado.Nome);
+        }
+
+        private Amigo SalvarAmigoTest(Amigo amigo) {
+            repositorio.Salvar(amigo);
+            return repositorio.Buscar(amigo.Id);
+        }
+    }
+}
---
+++ Test/ServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using Core.Repository;
+using Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test {
+    class ServiceLocator {
+        private static Dictionary<Type, Type> Usuario = new Dictionary<Type, Type> {
+            [typeof(IAmigo)] = typeof(AmigoRepository)
+        };
+
+        internal static T GetInstanceOf<T>() {
+            return Activator.CreateInstance<T>();
+        }
+    }
+}
